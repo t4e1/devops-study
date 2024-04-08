0c088f3c10d27beefc055524c3e4050b80d6e3e7
t4e1 <jtw5738@naver.com>
2024-04-08 16:12:25 +0900

    2024.04.08 쿠버네티스 시작

--- vue/vueProject/src/App.vue
+++ vue/vueProject/src/App.vue
@@ -17,7 +17,10 @@
     // const response = await fetch(`http://localhost:5173/api/plus?num1=${num1.value}&num2=${num2.value}`);
 
     /* 도커 컨테이너 안에 네트워크 연결 후 (5173/api가 아니라 8011/api로 바꿔야 된다.) */
-    const response = await fetch(`http://localhost:8011/api/plus?num1=${num1.value}&num2=${num2.value}`);
+    // const response = await fetch(`http://localhost:8011/api/plus?num1=${num1.value}&num2=${num2.value}`);
+
+    /* 백엔드를 쿠버네티스를 활용한 워커 노드로 띄웠을 경우 (30001번 node port를 사용하여) */
+    const response = await fetch(`http://localhost:30001/plus?num1=${num1.value}&num2=${num2.value}`);
     const data = await response.json();
     result.value = data.sum;
   }
